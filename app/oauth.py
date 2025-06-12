5176a7ab9d85f8f12984d50cbd0f17c06c9a7ec4
= <=>
2025-06-12 12:49:36 +0800

    secured home route

--- app/oauth.py
+++ app/oauth.py
@@ -1,7 +1,12 @@
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, status
+
+from .database import get_db
 from .config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
+from .models import User
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
@@ -12,3 +17,23 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode.update({"exp": int(expire.timestamp())})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
+
+def get_current_user(token = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials"
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    user = db.query(User).filter(User.username == username).first()
+    if user is None:
+        raise credentials_exception
+    
+    return user
--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from .schemas import CreateUser, Token
 from .models import User
 from . import services
 from .hashing import get_hash_password, verify_password
-from .oauth import create_access_token
+from .oauth import create_access_token, get_current_user
 
 
 route = APIRouter()
@@ -51,3 +51,8 @@ def create_account(registerData: CreateUser, db: Session = Depends(get_db)):
     db.add(new_user)
     db.commit()
     return f"User {registerData.username} has been created!"
+
+
+@route.get("/home", status_code=status.HTTP_200_OK)
+def home(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    return "Home page!"
